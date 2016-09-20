4a103961c52883d07f49c20fd62062c4d21dd714
jpmccrea <johnpatmccrea@aol.com>
2016-09-20 16:03:06 -0400

    yo

---
+++ r-intro.R
@@ -0,0 +1,28 @@
+2 = 3
+2==3
+2<3 | 2>3
+2<3 & 2>3
+cieling(pi)
+floor(pi)
+factorial(5)
+choose(4,2)
+5:100
+A = c(1,2,3,4,5); A
+B = c(6,7,8,9,10); A
+C = c(a,B,"George"); C
+D = rep(1,10); D
+E = rep(c(1,2,3,4,5),5); E
+G = rep(c(1,2,3,4),each=5); G
+H = seq(1,10); H
+K = seq(10,2,by=-0.5); K
+str(K)
+mat2 = matrix(1)
+edit(mat2)
+.Last.value
+
+mat1 = matrix(sample(1:50,size = 25))
+foo = function(x) {
+  a = 1;
+  b = 1;
+  
+}
